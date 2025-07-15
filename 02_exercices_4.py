f50a014fc9038954678753fe5abf7c73179789a1
Merveil-Kakina <159161335+Merveil-Kakina@users.noreply.github.com>
2025-07-15 14:54:06 +0100

    Add files via upload

---
+++ 02_exercices_4.py
@@ -0,0 +1,17 @@
+"""Énoncé :
+Demander trois notes (sur 20), calculer la moyenne et dire si l’étudiant est reçu (moyenne ≥ 10) ou non.
+"""
+
+
+note1 = float(input("Première note : "))
+note2 = float(input("Deuxième note : "))
+note3 = float(input("Troisième note : "))
+
+moyenne = (note1 + note2 + note3) / 3
+
+print(f"Moyenne : {moyenne:.2f}")
+
+if moyenne >= 10:
+    print("L’étudiant est reçu.")
+else:
+    print("L’étudiant n’est pas reçu.")
